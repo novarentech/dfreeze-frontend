60af6f0f8cadbeb583f34cc8349ae6e78c236223
Ahmad Luthfi Abdillah <ahmadluthfiabdillah@mail.ugm.ac.id>
2026-04-16 16:33:51 +0700

    feat: implement SEO component, sitemap integration, and robots.txt for improved site indexing

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,19 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 import "dotenv/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: process.env.PUBLIC_SITE_URL',
   output: 'server',
 
   adapter: node({
     mode: 'standalone',
   }),
 
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()],
