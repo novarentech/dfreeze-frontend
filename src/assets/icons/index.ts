9b55c175b7138e0540b0822f5be8b8573d53b069
Ahmad Luthfi Abdillah <ahmadluthfiabdillah@mail.ugm.ac.id>
2026-04-18 00:42:55 +0700

    feat: add Facilities section and import new SVG assets to index page

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,17 @@
+import loveIcon from "./love.svg?url";
+import graphIcon from "./graph.svg?url";
+import sanitizerIcon from "./sanitizer.svg?url";
+import shieldIcon from "./shield.svg?url";
+import thumbUpIcon from "./thumb-up.svg?url";
+import facebookIcon from "./facebook.svg?url";
+import mapsIcon from "./maps.svg?url";
+
+export {
+  loveIcon,
+  graphIcon,
+  sanitizerIcon,
+  shieldIcon,
+  thumbUpIcon,
+  facebookIcon,
+  mapsIcon
+};
